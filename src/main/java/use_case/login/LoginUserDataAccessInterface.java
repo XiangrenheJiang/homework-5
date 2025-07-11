72fd44046a360e4ba406f37cd07a89719c152d95
xiangrenhejiang <karryjiang0515@gmail.com>
2025-07-10 21:15:16 -0400

    Complete Task 2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set the name of the current user.
+     * @param name the name to set for the current user.
+     */
+    void setCurrentUser(String name);
 }
